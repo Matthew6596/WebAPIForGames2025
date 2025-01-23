aaff65707e656c63cd1e0b70cd01e109ff0a3208
Matthew6596 <114946013+Matthew6596@users.noreply.github.com>
2025-01-23 08:21:19 -0500

    added code from yesterday class

--- app.js
+++ app.js
@@ -1,42 +1,47 @@
-const express = require("express");
+const express = require("express"); //install w/ npm install express mongoose body-parser --save
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
 const path = require("path");
 
 const app = express();
-const port = 3000;
+const port = process.env.port||3000;
 
-function getpath(f){
-    return path.join(__dirname,"public",f);
-}
+//Helper for getting files in public folder dir
+function ppath(p){return path.join(__dirname,"public",p);}
 
-//Middleware to serve static data
+//Middleware for sending static data
 app.use(express.static(path.join(__dirname,"public")));
-//
+//Set up middleware to parse json requests
+app.use(bodyParser.json());
 
-//Routes
+//MongoDB connection setup
+const mongoURI = "mongodb://localhost:27017/crudapp"; //store const uri
+mongoose.connect(mongoURI); //connect
+const db = mongoose.connection; //store connection
+db.on("error",console.error.bind(console,"MongoDB connection error")); //connection error handling
+db.once("open",()=>{console.log("Connected to MongoDB Database")})
+
+//Setup Mongoose Schema
+const foodSchema = new mongoose.Schema({
+    name:String,
+    foodType:String,
+    ranking:Number
+});
+const Food = mongoose.model("Food",foodSchema,"fooddata");
+
+//=====Routes=====
 app.get("/",(req,res)=>{
-    res.sendFile(getpath("index.html"));
+    res.sendFile(ppath("index.html"));
 });
-app.get("/testjson",(req,res)=>{
-    res.sendFile(getpath("json/games.json"));
+app.get("/food",async(req,res)=>{
+    try{
+        const food = await Food.find();
+        res.json(food);
+    }catch(e){res.sendStatus(500).json({error:"Failed to get food."});}
 });
-app.get("/login",(req,res)=>{
-    res.sendFile(getpath("login.html"));
-});
-app.get("/additem",(req,res)=>{
-    res.sendFile(getpath("additem.html"));
-});
-app.get("/statuscode",(req,res)=>{
-    res.sendStatus(402); //api.weather.gov/gridpoints/TOP
-});
+//=====End-Routes=====
 
-/*setTimeout(()=>{
-    console.log("Hi, 2 seconds passed.");
-},2000);
-setTimeout(()=>{
-    console.log("Hi, now.");
-},0);*/
-
+//Starts server
 app.listen(port,()=>{
-    console.log("Server is running on port: "+port);
+    console.log(`Server is running on port ${port}`);
 });
-
